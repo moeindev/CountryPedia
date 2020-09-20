74927e7338421944f00c81e1261e8a2f3b05eca4
Moein <moeinabdi77@gmail.com>
2020-09-20 20:47:59 +0430

    add CountryViewModel

---
+++ app/src/main/java/ir/moeindeveloper/countrypedia/viewModel/CountryViewModel.kt
@@ -0,0 +1,29 @@
+package ir.moeindeveloper.countrypedia.viewModel
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import ir.moeindeveloper.countrypedia.data.model.local.Country
+import ir.moeindeveloper.countrypedia.data.repository.CountryRepository
+import ir.moeindeveloper.countrypedia.util.network.Resource
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+class CountryViewModel @ViewModelInject constructor(private val repository: CountryRepository): ViewModel() {
+
+    private val _countries = MutableLiveData<Resource<List<Country>>>()
+    val countries: LiveData<Resource<List<Country>>> get() = _countries
+
+
+    fun loadCountries() {
+        viewModelScope.launch {
+            repository.getCountries()
+                .collect {
+                    _countries.value = it
+                }
+        }
+    }
+
+}
