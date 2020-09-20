7dc982b8e18539ebd2e9ee5303c4022306a97689
Moein <moeinabdi77@gmail.com>
2020-09-20 20:48:25 +0430

    init lingVer

--- app/src/main/java/ir/moeindeveloper/countrypedia/CountryPedia.kt
+++ app/src/main/java/ir/moeindeveloper/countrypedia/CountryPedia.kt
@@ -1,8 +1,14 @@
 package ir.moeindeveloper.countrypedia
 
 import android.app.Application
+import com.yariksoffice.lingver.Lingver
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class CountryPedia: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Lingver.init(this,"en")
+    }
 }
